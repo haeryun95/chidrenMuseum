7e0a34aa96c96f7e41e9694327acebba63fbf391
Haeryun95 <haeryun805@gmail.com>
2026-03-24 16:47:42 +0900

    20260324

--- js/customSwiperSettings.js
+++ js/customSwiperSettings.js
@@ -1,15 +1,17 @@
 const mainVisualSlider = new Swiper(".mainVisualSlider", {
-  loop: true,
-  speed: 800,
-  slidesPerView: 1,
-  spaceBetween: 0,
-
-  navigation: {
-    nextEl: ".mainVisualNext",
-    prevEl: ".mainVisualPrev",
-  },
-  pagination: {
-    el: ".mainVisualPagination",
-    clickable: true,
-  },
+    loop: true,
+    speed: 800,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    autoplay: {
+        delay: 4000,
+    },
+    navigation: {
+        nextEl: ".mainVisualNext",
+        prevEl: ".mainVisualPrev",
+    },
+    pagination: {
+        el: ".mainVisualPagination",
+        clickable: true,
+    },
 });
