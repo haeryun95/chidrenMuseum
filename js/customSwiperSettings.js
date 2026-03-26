cd8ae45fb802487ba08f82a1a61f8a21c20fec26
Haeryun95 <haeryun805@gmail.com>
2026-03-26 16:04:46 +0900

    오후 20260326

--- js/customSwiperSettings.js
+++ js/customSwiperSettings.js
@@ -15,3 +15,21 @@ const mainVisualSlider = new Swiper(".mainVisualSlider", {
         clickable: true,
     },
 });
+const exhibitSliderSlider = new Swiper(".exhibitSlider", {
+    loop: true,
+    speed: 800,
+    slidesPerView: 2,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 4000,
+    },
+});
+const educationSlider = new Swiper(".educationSlider", {
+    loop: true,
+    speed: 800,
+    slidesPerView: 2,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 4100,
+    },
+});
