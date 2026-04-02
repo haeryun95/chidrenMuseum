ea7e2dccef0a286de3bb7e9de652cfce58794155
Haeryun95 <haeryun805@gmail.com>
2026-04-02 13:23:51 +0900

    20260402

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,11 @@
 const $familySiteBtn = $(".familySiteBtn");
-const $familySubMenu = $(".familySiteArea .subMenu");
+const $footerSubMenu = $(".familySiteBtn .subMenu");
 
 $familySiteBtn.on("click", function () {
     $(this).toggleClass("selected");
-    $familySubMenu.stop().slideToggle(200);
+    if ($(this).hasClass("selected")) {
+        $footerSubMenu.slideDown(100);
+    } else {
+        $footerSubMenu.slideUp(100);
+    }
 });
